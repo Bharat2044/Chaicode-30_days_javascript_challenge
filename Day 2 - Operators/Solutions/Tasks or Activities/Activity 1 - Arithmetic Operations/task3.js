17abca1f5d2f642c04d1cb7146d473de0e6208bd
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:07:59 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 1 - Arithmetic Operations/task3.js
@@ -0,0 +1,11 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 1 - Arithmetic Operations
+ *  - Task 3: Write a program to multiply two numbers and log the result to the console.
+ */
+
+
+let product = 5 * 3;
+
+console.log(`Product: ${product}`); // Output: Product: 15
