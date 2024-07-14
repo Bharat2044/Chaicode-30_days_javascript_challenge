d5b17eca5dfca6fce829272b0eaab10bb75b4233
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:07 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 1 - Arithmetic Operations/task5.js
@@ -0,0 +1,11 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 1 - Arithmetic Operations
+ *  - Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+ */
+
+
+let remainder = 10 % 3;
+
+console.log(`Remainder: ${remainder}`); // Output: Remainder: 1
