229dffb986c39588f5283f5bb160709df4db8573
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:04 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 1 - Arithmetic Operations/task4.js
@@ -0,0 +1,11 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 1 - Arithmetic Operations
+ *  - Task 4: Write a program to divide two numbers and log the result to the console.
+ */
+
+
+let quotient = 10 / 2;
+
+console.log(`Quotient: ${quotient}`); // Output: Quotient: 5
