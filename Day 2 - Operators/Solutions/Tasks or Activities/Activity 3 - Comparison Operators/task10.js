257a079fb27658304c0ba50e12b06f075cfadadd
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:26 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 3 - Comparison Operators/task10.js
@@ -0,0 +1,14 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 3 - Comparison Operators
+ *  - Task 10: Write a program to compare two numbers using `==` and `===` and log the result to the console.
+ */
+
+
+let num1 = 5;
+let num2 = '5';
+
+console.log(`num1 == num2: ${num1 == num1}`); // Output: num1 == num2: true
+
+console.log(`num1 === num2: ${num1 === num2}`); // Output: num1 === num2: false
