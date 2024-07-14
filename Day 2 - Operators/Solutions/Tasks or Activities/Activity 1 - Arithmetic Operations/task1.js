72c952850faa9e12a10f2c888f7e4500d22cccef
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:07:52 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 1 - Arithmetic Operations/task1.js
@@ -0,0 +1,11 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 1 - Arithmetic Operations
+ *  - Task 1: Write a program to add two numbers and log the result to the console.
+ */
+
+
+let sum = 5 + 3;
+
+console.log(`Sum: ${sum}`); // Output: Sum: 8
