ef8359ab17d3e92916fcb5a44d61c46ef31f6eb4
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:15:39 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 1 - Variable Declaration/task1.js
@@ -0,0 +1,8 @@
+/**
+ * Activity 1: Variable Declaration
+ *  - Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+*/
+
+var numberVar = 42;
+
+console.log('numberVar:', numberVar); // Output: numberVar: 42
