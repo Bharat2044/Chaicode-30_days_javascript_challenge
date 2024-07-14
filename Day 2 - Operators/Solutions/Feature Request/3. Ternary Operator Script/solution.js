797cbfd0280aa958eaf7886f0ccc39e40636062e
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:07:48 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Feature Request/3. Ternary Operator Script/solution.js
@@ -0,0 +1,14 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Feature Request: Ternary Operator Script
+ *  - Write a script that uses the ternary operator to determine if a number is positive or negative and logs the result.
+ */
+
+
+
+let num = 5;
+
+let result = num >= 0 ? "Positive" : "Negative";
+
+console.log(`The number ${num} is ${result}.`); // Output: The number 5 is Positive.
