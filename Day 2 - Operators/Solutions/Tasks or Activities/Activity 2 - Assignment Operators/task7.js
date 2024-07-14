103eb45e1fc9aa87a34eb0e8b0f42c68f5d0454f
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:15 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 2 - Assignment Operators/task7.js
@@ -0,0 +1,12 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 2 - Assignment Operators
+ *  - Task 7: Use the `-=` operator to subtract a number from a variable and log the result to the console.
+ */
+
+
+let b = 20;
+b -= 7;
+
+console.log(`b after -= 7: ${b}`); // Output: b after -= 7: 13
