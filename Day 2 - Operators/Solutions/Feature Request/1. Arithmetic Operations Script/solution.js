1f13997adcab8cfc2f971ca401c07eafb9a70d20
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:07:41 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Feature Request/1. Arithmetic Operations Script/solution.js
@@ -0,0 +1,16 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Feature Request: Arithmetic Operations Script
+ *  - Write a script that performs basic arithmetic operations (addition, subtraction, multiplication, division, remainder) on two num2ers and logs the results.
+ */
+
+
+let num1 = 12;
+let num2 = 4;
+
+console.log(`Addition: ${num1 + num2}`);       // Output: Addition: 16
+console.log(`Subtraction: ${num1 - num2}`);    // Output: Subtraction: 8
+console.log(`Multiplication: ${num1 * num2}`); // Output: Multiplication: 48
+console.log(`Division: ${num1 / num2}`);       // Output: Division: 3
+console.log(`Remainder: ${num1 % num2}`);      // Output: Remainder: 0
