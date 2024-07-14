c5d548c1a68c5f06d7748e15f52a201246020e20
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:23 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 3 - Comparison Operators/task09.js
@@ -0,0 +1,12 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 3 - Comparison Operators
+ *  - Task 9: Write a program to compare two numbers using `>=` and `<=` and log the result to the console.
+ */
+
+
+let num1 = 10, num2 = 15;
+
+console.log(`num1 >= num2: ${num1 >= num2}`); // Output: num1 >= num2: false
+console.log(`num1 <= num2: ${num1 <= num2}`); // Output: num1 <= num2: true
