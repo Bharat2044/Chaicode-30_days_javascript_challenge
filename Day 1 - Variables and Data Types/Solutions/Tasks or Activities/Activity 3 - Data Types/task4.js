91570db0870075a3d1c508f9e05bfe2e26469e2f
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:21:07 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 3 - Data Types/task4.js
@@ -0,0 +1,25 @@
+/**
+ * Day 1 - Variables and Data Types
+ * 
+ * Activity 3: Data Types 
+ *  - Task 4: Create variables of different data types and log each variable's type using the typeof operator.
+ */
+
+
+let num = 25; 
+let str = "JavaScript";
+let bool = false;
+let und = undefined;
+let nul = null;
+let obj = { name: "Bharat" };
+let arr = [1, 2, 3, 4];
+
+console.log(`num: ${num}, type: ${typeof num}`); // Output: num: 25, type: number
+console.log(`str: ${str}, type: ${typeof str}`); // Output: str: JavaScript, type: string
+console.log(`bool: ${bool}, type: ${typeof bool}`); // Output: bool: false, type: boolean
+console.log(`und: ${und}, type: ${typeof und}`); // Output: und: undefined, type: undefined
+console.log(`nul: ${nul}, type: ${typeof nul}`); // Output: nul: null, type: object
+console.log(`obj: ${obj}, type: ${typeof obj}`); // Output: obj: [object Object], type: object
+console.log(`arr: ${arr}, type: ${typeof arr}`); // Output: arr: 1,2,3,4, type: object
+
+
