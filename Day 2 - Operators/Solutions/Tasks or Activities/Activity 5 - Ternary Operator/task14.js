b747bb92cb9ae8378cc31971d48533640fe3ce1f
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:40 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 5 - Ternary Operator/task14.js
@@ -0,0 +1,13 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 5 - Ternary Operator
+ *  - Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+ */
+
+
+let number = -5;
+
+let result = number >= 0 ? 'Positive' : 'Negative';
+
+console.log(`The number is: ${result}`); // Output: The number is: Negative
