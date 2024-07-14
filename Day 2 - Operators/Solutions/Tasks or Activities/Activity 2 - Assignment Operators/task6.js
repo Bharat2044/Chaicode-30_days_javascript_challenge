cc3927c41ade32078731c59e66b39b35f83a342f
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:11 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 2 - Assignment Operators/task6.js
@@ -0,0 +1,12 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 2 - Assignment Operators
+ *  - Task 6: Use the `+=` operator to add a number to a variable and log the result to the console. 
+ */
+
+
+let a = 10;
+a += 5;
+
+console.log(`a after += 5: ${a}`); // Output: a after += 5: 15
