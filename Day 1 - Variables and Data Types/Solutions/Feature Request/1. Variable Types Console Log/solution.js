2bbcb0dc04191ad5a1643771b2f4a2a08ca02d78
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:15:31 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Feature Request/1. Variable Types Console Log/solution.js
@@ -0,0 +1,20 @@
+/**
+ * Variable Types Console Log
+ *  - Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+ */
+
+var numVar = 10;
+let strLet = "Hello";
+const boolConst = true;
+let undefinedLet;
+let nullVar = null;
+let objLet = { key: "value" };
+let arrLet = [1, 2, 3];
+
+console.log(`numVar: ${numVar}, type: ${typeof numVar}`);
+console.log(`strLet: ${strLet}, type: ${typeof strLet}`);
+console.log(`boolConst: ${boolConst}, type: ${typeof boolConst}`);
+console.log(`undefinedLet: ${undefinedLet}, type: ${typeof undefinedLet}`);
+console.log(`nullVar: ${nullVar}, type: ${typeof nullVar}`);
+console.log(`objLet: ${objLet}, type: ${typeof objLet}`);
+console.log(`arrLet: ${arrLet}, type: ${typeof arrLet}`);
