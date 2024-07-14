009676154c0c679d27b0a7553d4e20ffc586fe2e
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:21:10 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 4 - Reassigning Variables/task5.js
@@ -0,0 +1,13 @@
+/**
+ * Day 1 - Variables and Data Types
+ * 
+ * Activity 4: Reassigning Variables
+ *  - Task 5: Declare a variable using `let`, assign it an initial value, reassign a new value, and log both values to the console.
+ */
+
+
+let reassignLet = "Bharat Kumar";
+console.log('reassignLet (initial):', reassignLet); // Output: reassignLet (initial): Bharat Kumar
+
+reassignLet = "Deepak Singh";
+console.log('reassignLet (new):', reassignLet); // Output: reassignLet (new): Deepak Singh
