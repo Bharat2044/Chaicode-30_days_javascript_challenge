5becace2da2c0545081999c8aed9e6e9f5693a68
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:20:55 +0530

    assignment solution code added

--- Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 1 - Variable Declaration/task1.js
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 1 - Variable Declaration/task1.js
@@ -1,4 +1,6 @@
 /**
+ * Day 1 - Variables and Data Types
+ * 
  * Activity 1: Variable Declaration
  *  - Task 1: Declare a variable using var, assign it a number, and log the value to the console.
 */
