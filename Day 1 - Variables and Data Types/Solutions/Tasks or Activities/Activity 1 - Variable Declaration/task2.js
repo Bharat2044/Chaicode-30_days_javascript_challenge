e228d77e72ec30ddb858619cb1de0efb81bca08a
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:15:45 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 1 - Variable Declaration/task2.js
@@ -0,0 +1,8 @@
+/**
+ * Activity 1: Variable Declaration
+ *  - Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+*/
+
+let stringLet = "Hello, World!";
+
+console.log('stringLet:', stringLet); // Output: stringLet: Hello, World!
