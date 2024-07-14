59c8eb8167c9fc0cefd8e8840acebfd8a79beec2
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:21:03 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 2 - Constant Declaration/task3.js
@@ -0,0 +1,11 @@
+/**
+ * Day 1 - Variables and Data Types
+ * 
+ * Activity 2: Constant Declaration
+ *  - Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+ */
+
+const booleanConst = true;
+
+console.log('booleanConst:', booleanConst); // Output: booleanConst: true
+
