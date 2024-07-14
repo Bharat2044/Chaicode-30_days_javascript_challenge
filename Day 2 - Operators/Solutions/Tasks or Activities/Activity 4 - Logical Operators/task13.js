e278ad63fc0404a2f05315061ff8659377e4ca53
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:08:36 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 4 - Logical Operators/task13.js
@@ -0,0 +1,17 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 4 - Logical Operators
+ *  - Task 13: Write a program that uses the `!` operator to negate a condition and log the result to the console.
+ */
+
+
+let x = 5, y = 10;
+
+console.log(`!(x > 10): ${!(x > 10)}`); // Output: !(x > 10): true
+
+
+let a = true, b = false;
+
+console.log(`!${a}: ${!a}`); // Output: !a: false
+console.log(`!${b}: ${!b}`); // Output: !b: true
