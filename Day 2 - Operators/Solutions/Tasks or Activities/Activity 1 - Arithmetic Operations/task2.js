03c82b616f2021400ea491691b7e254d23293bee
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 00:07:55 +0530

    assignment solution code added

---
+++ Day 2 - Operators/Solutions/Tasks or Activities/Activity 1 - Arithmetic Operations/task2.js
@@ -0,0 +1,11 @@
+/**
+ * Day 2 - Operators
+ * 
+ * Activity 1 - Arithmetic Operations
+ *  - Task 2: Write a program to subtract two numbers and log the result to the console.
+ */
+
+
+let difference = 10 - 6;
+
+console.log(`Difference: ${difference}`); // Output: Difference: 4
