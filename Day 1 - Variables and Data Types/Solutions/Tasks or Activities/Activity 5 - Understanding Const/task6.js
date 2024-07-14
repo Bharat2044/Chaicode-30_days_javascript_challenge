5a20cdd7f3e9a5b6568c5f872783d8a2de5dfa85
Bharat2044 <bk9955204456@gmail.com>
2024-07-14 23:21:13 +0530

    assignment solution code added

---
+++ Day 1 - Variables and Data Types/Solutions/Tasks or Activities/Activity 5 - Understanding Const/task6.js
@@ -0,0 +1,13 @@
+/**
+ * Day 1 - Variables and Data Types
+ * 
+ * Activity 5: Understanding `const`
+ *  - Task 6: Try reassigning a variable declared with `const` and observe the error.
+ */
+
+
+const reassignConst = "Bharat";
+console.log('reassignConst:', reassignConst); // Output: reassignConst: Bharat
+
+// Uncomment the following line to observe the error
+// reassignConst = "new value"; // This will cause an error: TypeError: Assignment to constant variable.
