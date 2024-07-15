a83d44b1c1abab726b53ee4e8ae61914dacc2811
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 17:24:18 +0530

    assignment solution code added

---
+++ Day 3 - Control Structures/Solutions/Feature Request/1. Number Check Script/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Day 3 - Control Structures
+ * 
+ * Feature Request: Number Check Script
+ *  - Write a script that checks if a number is positive, negative, or zero using if-else statements and logs the result.
+ */
+
+
+// Declare a variable to store the number
+let num = -5;
+
+// Check if the number is positive, negative, or zero
+if (num > 0) { // If the number is greater than 0
+    console.log(`${num} is a positive number.`);
+} else if (num < 0) { // If the number is less than 0
+    console.log(`${num} is a negative number.`);
+} else { // If the number is 0
+    console.log(`${num} is zero.`);
+}
