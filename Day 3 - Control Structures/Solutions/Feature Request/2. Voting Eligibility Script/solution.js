70dc044cc2331fff4f1db67b5127b841b596a1b0
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 17:24:22 +0530

    assignment solution code added

---
+++ Day 3 - Control Structures/Solutions/Feature Request/2. Voting Eligibility Script/solution.js
@@ -0,0 +1,17 @@
+/**
+ * Day 3 - Control Structures
+ * 
+ * Feature Request: Voting Eligibility Script
+ *  - Create a script to check if a person is eligible to vote based on their age and log the result.
+ */
+
+
+// Declare a variable to store the age
+let age = 16;
+
+// Check if the person is eligible to vote based on their age
+if (age >= 18) { // If the age is greater than or equal to 18
+    console.log("You are eligible to vote.");
+} else { // If the age is less than 18
+    console.log("You are not eligible to vote.");
+}
