c493e33a281f414a79ee679e63845c216d32ba1c
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 17:24:58 +0530

    assignment solution code added

---
+++ Day 3 - Control Structures/Solutions/Tasks or Activities/Activity 4 - Conditional (Ternary) Operator/task6.js
@@ -0,0 +1,16 @@
+/**
+ * Day 3 - Control Structures
+ *
+ * Activity 4 - Conditional (Ternary) Operator
+ *  - Task 6: Write a program that uses the ternary operator to check if a number is even or odd and log the result to the console.
+ */
+
+
+// Declare a variable and assign a number to it
+let num = 7;
+
+// Check if the number is even or odd using the ternary operator
+let result = num % 2 === 0 ? `${num} is an even number.` : `${num} is an odd number.`;
+
+// Print the result to the console
+console.log(result); // Output: 7 is an odd number.
