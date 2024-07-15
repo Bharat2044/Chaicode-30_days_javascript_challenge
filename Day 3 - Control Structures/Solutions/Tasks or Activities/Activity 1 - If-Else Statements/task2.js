5011a2c636f1100838ed1307d32814c9ea44ae7e
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 17:24:42 +0530

    assignment solution code added

---
+++ Day 3 - Control Structures/Solutions/Tasks or Activities/Activity 1 - If-Else Statements/task2.js
@@ -0,0 +1,17 @@
+/**
+ * Day 3 - Control Structures
+ *
+ * Activity 1 - If-Else Statements
+ *  - Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+ */
+
+
+// Declare a variable and assign an age to it
+let age = 20;
+
+// Check if the person is eligible to vote based on their age
+if (age >= 18) { // If the age is greater than or equal to 18
+    console.log("You are eligible to vote.");
+} else { // If the age is less than 18
+    console.log("You are not eligible to vote.");
+}
