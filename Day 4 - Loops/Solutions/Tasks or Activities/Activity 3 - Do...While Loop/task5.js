f7bdc1039c364faef888e8aae5771549d0f9a052
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:50 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 3 - Do...While Loop/task5.js
@@ -0,0 +1,14 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 3: Do...While Loop
+ *  - Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
+ */
+
+
+let i = 1;
+
+do {
+    console.log(i);
+    i++;
+} while (i <= 5);
