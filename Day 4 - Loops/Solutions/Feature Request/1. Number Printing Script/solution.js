318e34efac9eea73305c6f524f056c72b86f6c4d
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:14 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Feature Request/1. Number Printing Script/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Feature Request: Number Printing Script
+ *  - Write a script that prints numbers from 1 to 10 using a for loop and a while loop.
+ */
+
+
+// Using a for loop
+console.log('Using a for loop:');
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// Using a while loop
+console.log('\nUsing a while loop:');
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
