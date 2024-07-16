1dfaa6b7c703b4369d892cbbecd6a601921ad387
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:23 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Feature Request/4. Sum Calculation Script/solution.js
@@ -0,0 +1,17 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Feature Request: Sum Calculation Script
+ *  - Write a script that calculates the sum of numbers from 1 to 10 using a while loop.
+ */
+
+
+let sum = 0;
+let i = 1;
+
+while (i <= 10) {
+    sum += i;
+    i++;
+}
+
+console.log(`Sum: ${sum}`);
