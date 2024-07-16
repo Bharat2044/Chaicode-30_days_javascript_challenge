df8ae2cd86300db99a1d56c70a9e664ed0468e46
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:41 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 2 - While Loop/task3.js
@@ -0,0 +1,17 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 2: While Loop
+ *  - Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+ */
+
+
+let sum = 0;
+let i = 1;
+
+while (i <= 10) {
+    sum += i;
+    i++;
+}
+
+console.log(`Sum: ${sum}`);
