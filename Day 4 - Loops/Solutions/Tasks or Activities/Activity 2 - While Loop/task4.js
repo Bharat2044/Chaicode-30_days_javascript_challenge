50b35b1d8a1a69acf5640fc2be276afc7b85ca2f
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:46 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 2 - While Loop/task4.js
@@ -0,0 +1,14 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 2: While Loop
+ *  - Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+ */
+
+
+let i = 10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+}
