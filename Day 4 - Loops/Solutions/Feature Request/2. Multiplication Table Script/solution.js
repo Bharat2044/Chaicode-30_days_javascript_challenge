406870318cde8be4e94c675f995d40e07b848c16
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:09 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Feature Request/2. Multiplication Table Script/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Feature Request: Multiplication Table Script
+ *  - Create a script that prints the multiplication table of 5 using a for loop.
+ */
+
+
+let num = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${num} x ${i} = ${num * i}`);
+}
