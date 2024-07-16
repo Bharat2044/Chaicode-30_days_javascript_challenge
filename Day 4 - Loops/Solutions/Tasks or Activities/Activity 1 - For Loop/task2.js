9da8a72715403cb66bcf6c1d3476e954053d565b
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:37 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 1 - For Loop/task2.js
@@ -0,0 +1,14 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 1: For Loop
+ *  - Task 2: Write a program to print the multiplication table of 5 using a for loop.
+ */
+
+
+
+let n = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${n} x ${i} = ${n * i}`);
+}
