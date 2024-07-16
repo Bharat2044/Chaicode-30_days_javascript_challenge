1be975e4f82644bde1d747a80d1114ad90f0d987
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:09:03 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 5 - Loop Control Statements/task9.js
@@ -0,0 +1,16 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 5: Loop Control Statements
+ *  - Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the `break` statement.
+ */
+
+
+for (let i = 1; i <= 10; i++) {
+
+    if (i === 7) {
+        break;
+    }
+
+    console.log(i);
+}
