7287675b905e3c4ad12c563b471eb7b60d0149cb
Bharat2044 <bk9955204456@gmail.com>
2024-07-16 18:08:28 +0530

    assignment solution code added

---
+++ Day 4 - Loops/Solutions/Tasks or Activities/Activity 1 - For Loop/task1.js
@@ -0,0 +1,12 @@
+/**
+ * Day 4 - Loops
+ * 
+ * Activity 1: For Loop
+ *  - Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+ */
+
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
