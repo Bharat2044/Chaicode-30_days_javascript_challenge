ba16d43c778a98463ea29f32fe1c22b126804124
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:13 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 2 - Function Expression/task3.js
@@ -0,0 +1,14 @@
+/**
+ * Day 5 - Functions
+ *
+ * Activity 2: Function Expression
+ *  - Task 3: Find the maximum of two numbers.
+ */
+
+
+// Function expression
+const findMax = function (num1, num2) {
+  return num1 > num2 ? num1 : num2;
+};
+
+console.log(findMax(5, 10)); // 10
