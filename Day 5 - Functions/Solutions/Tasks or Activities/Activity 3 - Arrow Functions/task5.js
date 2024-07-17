e0ddc356cc16de6647c800d5dc7f689a440beeee
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:22 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 3 - Arrow Functions/task5.js
@@ -0,0 +1,12 @@
+/**
+ * Day 5 - Functions
+ * 
+ * Activity 3: Arrow Functions
+ *  - Task 5: Sum of two numbers.
+ */
+
+
+// Arrow function
+const sum = (num1, num2) => num1 + num2;
+
+console.log("Sum = ", sum(5, 10)); // 15
