4036504b1c7f4ed524e38d2032fdcf86ed533b2d
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:18 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 2 - Function Expression/task4.js
@@ -0,0 +1,14 @@
+/**
+ * Day 5 - Functions
+ *
+ * Activity 2: Function Expression
+ *  - Task 4: Concatenate two strings.
+ */
+
+
+// Function expression
+const concatenateStrings = function (str1, str2) {
+  return str1 + str2;
+};
+
+console.log(concatenateStrings("Hello, ", "World!"));   // Hello, World!
