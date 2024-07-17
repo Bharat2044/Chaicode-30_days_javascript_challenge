5de02ed4583e847adc5cab508f19081b1a47a88a
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:04 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 1 - Function Declaration/task1.js
@@ -0,0 +1,17 @@
+/**
+ * Day 5 - Functions
+ *
+ * Activity 1: Function Declaration
+ *  - Task 1: Check if a number is even or odd.
+ */
+
+
+function isEvenOrOdd(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} is an even number.`);
+  } else {
+    console.log(`${num} is an odd number.`);
+  }
+}
+
+isEvenOrOdd(3);
