082366201fa18698e4ccce5136e01eee5425ce9b
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:19:51 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Feature Request/3. Concatenation Function Script/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Day 5 - Functions
+ * 
+ * Feature Request: Concatenation Function Script
+ *  - Concatenate two strings.
+ */
+
+
+function concatenateStrings(str1, str2) {
+  return str1 + str2;
+}
+
+console.log(concatenateStrings("Hello, ", "World!"));   // Hello, World!
