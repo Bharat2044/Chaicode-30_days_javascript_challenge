f7dce664e9384684b52bd2e9bde100de0e086ef2
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:19:38 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Feature Request/2. Square Calculation Function Script/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Day 5 - Functions
+ * 
+ * Feature Request: Square Calculation Function Script
+ *  - Calculate the square of a number.
+ */
+
+
+function square(num) {
+  return num * num;
+}
+
+console.log(square(5)); // 25
