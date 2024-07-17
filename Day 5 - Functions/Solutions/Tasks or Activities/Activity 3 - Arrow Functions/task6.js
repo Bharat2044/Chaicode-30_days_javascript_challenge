d0178966a7c2030bfe476657849ed7078f0b844f
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:26 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 3 - Arrow Functions/task6.js
@@ -0,0 +1,12 @@
+/**
+ * Day 5 - Functions
+ * 
+ * Activity 3: Arrow Functions
+ *  - Task 6: Check if a string contains a specific character.
+ */
+
+
+// Arrow function
+const containsChar = (str, char) => str.includes(char);
+
+console.log(containsChar("Hello", "e")); // true
