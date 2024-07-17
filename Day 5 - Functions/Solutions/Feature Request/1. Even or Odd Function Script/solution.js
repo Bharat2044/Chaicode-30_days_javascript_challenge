bbcd7a703bfd9a8e9e3ce6d41c91be2d0de2d591
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:19:33 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Feature Request/1. Even or Odd Function Script/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Day 5 - Functions
+ *
+ * Feature Request: Even or Odd Function Script
+ *  - Check if a number is even or odd.
+ */
+
+
+function evenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(evenOrOdd(5)); // Odd
