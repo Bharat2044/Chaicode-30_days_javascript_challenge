b25ebf7a19a938a292d98c04cf21578ce9400758
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:19:56 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Feature Request/4. Sum Calculation Arrow Function Script/solution.js
@@ -0,0 +1,12 @@
+/**
+ * Day 5 - Functions
+ * 
+ * Feature Request: Sum Calculation Arrow Function Script
+ *  - Sum of two numbers.
+ */
+
+
+// Arrow function
+const sum = (num1, num2) => num1 + num2;
+
+console.log("Sum = ", sum(5, 10)); // 15
