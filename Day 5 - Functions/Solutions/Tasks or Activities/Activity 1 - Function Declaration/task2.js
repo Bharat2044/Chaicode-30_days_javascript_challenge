084e0d723ddd4ed6672526a264849b98cb09a845
Bharat2044 <bk9955204456@gmail.com>
2024-07-17 22:20:09 +0530

    assignment solution code added

---
+++ Day 5 - Functions/Solutions/Tasks or Activities/Activity 1 - Function Declaration/task2.js
@@ -0,0 +1,13 @@
+/**
+ * Day 5 - Functions
+ *
+ * Activity 1: Function Declaration
+ *  - Task 2: Calculate the square of a number.
+ */
+
+
+function square(num) {
+  return num * num;
+}
+
+console.log(square(5));  // 25
