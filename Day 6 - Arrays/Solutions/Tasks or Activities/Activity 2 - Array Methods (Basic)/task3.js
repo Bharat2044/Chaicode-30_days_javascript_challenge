4df54770e27956c0c035267c552a9c62667a4751
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:09:45 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 2 - Array Methods (Basic)/task3.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 2: Array Methods (Basic)
+ *  - Task 3: Use the `push` method to add a new number to the end of the array and log the updated array.
+ */ 
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+// add a new number to the end of the array
+numbers.push(6);
+
+// printing the updated array
+console.log(numbers); // [1, 2, 3, 4, 5, 6]
