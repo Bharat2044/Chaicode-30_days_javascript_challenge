4ff8785c0ab1cef62217861c0d56b61642dba66a
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:24 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 5 - Multi-dimensional Arrays/task12.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 5: Multi-dimensional Arrays
+ *  - Task 12: Create a two-dimensional array (matrix) and log the entire array to the console.
+ */ 
+
+
+// create 2D array
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log(matrix);
