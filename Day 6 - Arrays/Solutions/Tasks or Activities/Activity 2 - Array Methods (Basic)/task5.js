b510add7815c78401637439c1a65716d22a7ee48
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:09:40 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 2 - Array Methods (Basic)/task5.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 2: Array Methods (Basic)
+ *  - Task 5: Use the `shift` method to remove the first element from the array and log the updated array.
+ */ 
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+// remove the first element from the array
+numbers.shift();
+
+// printing the updated array
+console.log(numbers); // [2, 3, 4, 5]
