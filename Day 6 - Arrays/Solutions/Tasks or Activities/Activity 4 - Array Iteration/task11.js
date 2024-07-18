d9efc0a91838af4aab127d60f15f830df0982b41
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:04 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 4 - Array Iteration/task11.js
@@ -0,0 +1,15 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 4: Array Iteration
+ *  - Task 11: Use the `forEach` method to iterate over the array and log each element to the console.
+ */ 
+
+
+// array creation
+let arr = [1, 2, 3, 4, 5];
+
+// iterating over the array using the forEach method
+arr.forEach(ele => {
+    console.log(ele);
+});
