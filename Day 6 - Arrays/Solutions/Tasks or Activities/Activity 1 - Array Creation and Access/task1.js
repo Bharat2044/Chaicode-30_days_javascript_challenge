739b3490e9d4c959e05606da024225c62092e62e
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:09:29 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 1 - Array Creation and Access/task1.js
@@ -0,0 +1,13 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 1: Array Creation and Access
+ *  - Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+ */
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers); // [1, 2, 3, 4, 5]
+
