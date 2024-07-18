62b5d4d9a7944e43496912f4af7d4a9a92087f24
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:09:55 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 3 - Array Methods (Intermediate)/task8.js
@@ -0,0 +1,17 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 3: Array Methods (Intermediate)
+ *  - Task 7: Use the `map` method to create a new array where each number is doubled and log the new array.
+ */ 
+
+
+
+// array creation
+let arr = [1, 2, 3, 4, 5];
+
+// using the `map` method to double each number
+let doubledArr = arr.map((num) => num * 2);
+
+// printing the new array
+console.log(doubledArr); // [2, 4, 6, 8, 10]
