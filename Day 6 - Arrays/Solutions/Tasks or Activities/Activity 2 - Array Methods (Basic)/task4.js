d84003d7d884dc4faf78236f126dbb5c0091b99d
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:09:34 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 2 - Array Methods (Basic)/task4.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 2: Array Methods (Basic)
+ *  - Task 4: Use the `pop` method to remove the last element from the array and log the updated array.
+ */ 
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+// remove the last element from the array
+numbers.pop();
+
+// printing the updated array
+console.log(numbers); // [1, 2, 3, 4]
