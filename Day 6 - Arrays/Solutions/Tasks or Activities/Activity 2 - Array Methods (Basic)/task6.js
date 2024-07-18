95268ec2662c3d18cc1af61680f87f8094ec8306
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:15 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 2 - Array Methods (Basic)/task6.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 2: Array Methods (Basic)
+ *  - Task 6: Use the `unshift` method to add a new number to the beginning of the array and log the updated array.
+ */ 
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+// add a new number to the beginning of the array
+numbers.unshift(99);
+
+// printing the updated array
+console.log(numbers); // [99, 1, 2, 3, 4, 5]
