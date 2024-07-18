0b5daeeb6fd217b842707dfc7142d9600894ee72
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:10 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 4 - Array Iteration/task10.js
@@ -0,0 +1,16 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 4: Array Iteration
+ *  - Task 10: Use a `for` loop to iterate over the array and log each element to the console.
+ */ 
+
+
+// array creation
+let arr = [1, 2, 3, 4, 5];
+
+// iterating over the array using a for loop
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
