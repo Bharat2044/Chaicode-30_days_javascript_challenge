eaca788b92450fa75fda6efb4417301a67889c98
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:20 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 5 - Multi-dimensional Arrays/task13.js
@@ -0,0 +1,21 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 5: Multi-dimensional Arrays
+ *  - Task 13: Access and log a specific element from the two-dimensional array.
+ */ 
+
+
+// create 2D array
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// Accessing specific element from the two-dimensional array.
+console.log(matrix[0][0]);  // 1
+console.log(matrix[0][2]);  // 3
+console.log(matrix[1][1]);  // 5
+console.log(matrix[2][2]);  // 9
+console.log(matrix[2][1]);  // 8
