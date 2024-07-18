407d5ed43fa0d6b87b36b361789947a527e88893
Bharat2044 <bk9955204456@gmail.com>
2024-07-18 15:10:29 +0530

    assignment solution code added

---
+++ Day 6 - Arrays/Solutions/Tasks or Activities/Activity 1 - Array Creation and Access/task2.js
@@ -0,0 +1,18 @@
+/**
+ * Day 6 - Arrays
+ * 
+ * Activity 1: Array Creation and Access
+ *  - Task 2: Access the first and last elements of the array and log them to the console.
+ */ 
+
+
+// array creation
+let numbers = [1, 2, 3, 4, 5];
+
+// access the first and last elements
+let firstElement = numbers[0];
+let lastElement = numbers[numbers.length - 1];
+
+// printing the first and last elements
+console.log(firstElement); // 1
+console.log(lastElement); // 5
