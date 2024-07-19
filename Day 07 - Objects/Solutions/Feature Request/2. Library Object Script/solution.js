09491ad5b974e89d73845969f88fee15c2edb942
Bharat2044 <bk9955204456@gmail.com>
2024-07-19 18:26:20 +0530

    assignment solution code added

---
+++ Day 07 - Objects/Solutions/Feature Request/2. Library Object Script/solution.js
@@ -0,0 +1,42 @@
+/**
+ * Day 7 - Objects
+ * 
+ * Feature Request: Library Object Script
+ *  - Define a library object with book objects and log details.
+ */
+
+
+// creating book objects
+let book1 = {
+    title: "JavaScript",
+    author: "Bharat",
+    year: 2021
+};
+
+let book2 = {
+    title: "Python",
+    author: "Deepak",
+    year: 2022
+};
+
+let book3 = {
+    title: "Java",
+    author: "Raju",
+    year: 2023
+};
+
+// creating library object with name and books (array of book objects)
+let library = {
+    name: "Coding Library",
+    books: [book1, book2, book3]
+};
+
+// printing the library object
+console.log(library);
+
+// Access and printing the library's name and book titles
+console.log(`Library Name: ${library.name}`);
+
+library.books.forEach(book => {
+    console.log(`Book Title: ${book.title}`);
+});
