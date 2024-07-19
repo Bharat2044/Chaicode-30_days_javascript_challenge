432ebcd9ed87f1eed61ce0943028655380f0ceb8
Bharat2044 <bk9955204456@gmail.com>
2024-07-19 18:26:38 +0530

    assignment solution code added

---
+++ Day 07 - Objects/Solutions/Tasks or Activities/Activity 1 - Object Creation and Access/task2.js
@@ -0,0 +1,18 @@
+/**
+ * Day 7 - Objects
+ *
+ * Activity 1: Object Creation and Access
+ *  - Task 2: Access and log the title and author properties.
+ */
+
+
+// creating book object with properties like title, author, and year
+let book = {
+    title: "JavaScript",
+    author: "Bharat",
+    year: 2021
+};
+
+// Access and printing the title and author properties
+console.log("Title: " + book.title);
+console.log("Author: " + book.author);
