0ca1969f45ae6958f155554876e550f5bdf6b257
Bharat2044 <bk9955204456@gmail.com>
2024-07-20 18:13:27 +0530

    assignment solurion code added

---
+++ Day 08 - ES6+ Features/Solutions/Tasks or Activities/Activity 1 - Template Literals/task2.js
@@ -0,0 +1,13 @@
+/**
+ * Day 8 - ES6+ Features
+ * 
+ * Activity 1 - Template Literals
+ *  - Task 2: Create a multi-line string using template literals and log it to the console.
+ */
+
+
+const multiLineString = `This is a
+multi-line string
+using template literals.`;
+
+console.log(multiLineString);
