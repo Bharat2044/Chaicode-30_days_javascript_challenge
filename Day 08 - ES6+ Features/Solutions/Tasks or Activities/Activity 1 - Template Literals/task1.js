6acb348ace681a82d7222839c332febd7a4cf59b
Bharat2044 <bk9955204456@gmail.com>
2024-07-20 18:13:23 +0530

    assignment solurion code added

---
+++ Day 08 - ES6+ Features/Solutions/Tasks or Activities/Activity 1 - Template Literals/task1.js
@@ -0,0 +1,13 @@
+/**
+ * Day 8 - ES6+ Features
+ * 
+ * Activity 1 - Template Literals
+ *  - Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+ */
+
+
+// create variables for a person's name and age
+let name = "Bharat";
+let age = 21;
+
+console.log(`My name is ${name} and I am ${age} years old.`); 
