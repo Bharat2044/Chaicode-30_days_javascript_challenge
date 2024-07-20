4f987e2b1405c2226ace9ff3aee482c6ba798c7f
Bharat2044 <bk9955204456@gmail.com>
2024-07-20 18:13:02 +0530

    assignment solurion code added

---
+++ Day 08 - ES6+ Features/Solutions/Feature Request/1. Template Literals Script/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Day 8 - ES6+ Features
+ * 
+ * Feature Request: Template Literals Script
+ *  - Write a script that demonstrates the use of template literals to create and log strings with embedded variables and multi-line strings.
+ */
+
+
+let name = "Bharat";
+let age = 25;
+
+let introduction = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(introduction);
+
+
+const multiLine = `This is the first line.
+And this is the second line.`;
+
+console.log(multiLine);
