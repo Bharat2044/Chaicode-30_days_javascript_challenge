5d6ba07a47a9fa45f4945412eb927cbecfd0e6f3
Bharat2044 <bk9955204456@gmail.com>
2024-07-20 18:13:31 +0530

    assignment solurion code added

---
+++ Day 08 - ES6+ Features/Solutions/Tasks or Activities/Activity 2 - Destructuring/task3.js
@@ -0,0 +1,13 @@
+/**
+ * Day 8 - ES6+ Features
+ * 
+ * Activity 2 - Destructuring
+ *  - Task 3: Use array destructuring to extract the first and second elements from an array of numbers and log them to the console.
+ */
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+let [first, second] = numbers;
+
+console.log(`First: ${first}, Second: ${second}`); // First: 1, Second: 2
