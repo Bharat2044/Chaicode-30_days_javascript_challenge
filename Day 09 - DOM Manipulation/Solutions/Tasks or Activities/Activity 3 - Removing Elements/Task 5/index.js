fe8f222a27a833e250a2423e0dd077d541111b2f
Bharat2044 <bk9955204456@gmail.com>
2024-07-21 15:19:59 +0530

    code added

---
+++ Day 09 - DOM Manipulation/Solutions/Tasks or Activities/Activity 3 - Removing Elements/Task 5/index.js
@@ -0,0 +1,13 @@
+/**
+ * Day 9 - DOM Manipulation
+ * 
+ * Activity 3 - Removing Elements
+ *  - Task 5: Remove an HTML element from the DOM.
+ */
+
+
+// Select the element to remove
+const element = document.querySelector('p');
+
+// Remove the element from the DOM
+element.remove();
