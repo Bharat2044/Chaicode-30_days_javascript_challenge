e179a22783530e4a5bb60f0a2211159c6b9a6649
Bharat2044 <bk9955204456@gmail.com>
2024-07-21 15:20:16 +0530

    code added

---
+++ Day 09 - DOM Manipulation/Solutions/Tasks or Activities/Activity 4 - Modifying Attributes and Classes/Task 7/index.js
@@ -0,0 +1,13 @@
+/**
+ * Day 9 - DOM Manipulation
+ * 
+ * Activity 4 - Modifying Attributes and Classes
+ *  - Task 7: Change an attribute (e.g., `src` of an `img`).
+ */
+
+
+// Select the `img` element
+const img = document.querySelector('img');
+
+// Change the `src` attribute of the `img`
+img.src = 'https://t3.ftcdn.net/jpg/06/10/71/64/360_F_610716498_li6BIgt75TXw8B4W89pbf3VtKgHNQkXo.jpg';
