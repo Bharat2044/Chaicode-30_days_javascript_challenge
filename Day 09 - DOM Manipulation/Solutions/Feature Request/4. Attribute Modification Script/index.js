ac44e0b6c0d8da7b8e47034f9df1f89bfec9eebf
Bharat2044 <bk9955204456@gmail.com>
2024-07-21 15:19:08 +0530

    code added

---
+++ Day 09 - DOM Manipulation/Solutions/Feature Request/4. Attribute Modification Script/index.js
@@ -0,0 +1,13 @@
+/**
+ * Day 9 - DOM Manipulation
+ * 
+ * Feature Request: Attribute Modification Script
+ *  - Change HTML element attributes.
+ */
+
+
+// Select the `img` element
+const img = document.querySelector('img');
+
+// Change the `src` attribute of the `img`
+img.src = 'https://t3.ftcdn.net/jpg/06/10/71/64/360_F_610716498_li6BIgt75TXw8B4W89pbf3VtKgHNQkXo.jpg';
