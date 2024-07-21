1c25bd401c5ee841269aaf6d56c53685a201ae99
Bharat2044 <bk9955204456@gmail.com>
2024-07-21 15:18:59 +0530

    code added

---
+++ Day 09 - DOM Manipulation/Solutions/Feature Request/3. Element Removal Script/index.js
@@ -0,0 +1,13 @@
+/**
+ * Day 9 - DOM Manipulation
+ * 
+ * Feature Request: Element Removal Script
+ *  - Remove an element from the DOM.
+ */
+
+
+// Select the element to remove
+const element = document.querySelector('p');
+
+// Remove the element from the DOM
+element.remove();
