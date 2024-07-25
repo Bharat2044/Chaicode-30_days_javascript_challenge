c1e2c256e91be7c130ccde7bbbb0c42135a93d73
Bharat2044 <bk9955204456@gmail.com>
2024-07-25 10:55:02 +0530

    assignment solution code added

---
+++ Day 13 - Modules/Solutions/Feature Request/1. Basic Module Script/mathModule.js
@@ -0,0 +1,19 @@
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function diff(a, b) {
+//     return a - b;
+// }
+
+// module.exports = { sum, diff };
+
+
+exports.sum = function sum(a, b) {
+    return a + b;
+}
+
+exports.diff = function diff(a, b) {
+    return a - b;
+}
