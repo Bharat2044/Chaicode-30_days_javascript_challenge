ec6318374af90fe56116a2d05f2cd04cefddbfda
Bharat2044 <bk9955204456@gmail.com>
2024-07-25 10:55:41 +0530

    assignment solution code added

---
+++ Day 13 - Modules/Solutions/Tasks or Activities/Activity 2 - Named and Default Exports/Task 3/mathModule.js
@@ -0,0 +1,33 @@
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// function diff(a, b) {
+//     return a - b;
+// }
+
+// function prod(a, b) {
+//     return a * b;
+// }
+
+// module.exports = { sum, diff, prod };
+
+// module.exports = {
+//     sum: sum,
+//     diff: diff,
+//     prod: prod
+// };
+
+
+exports.sum = function sum(a, b) {
+    return a + b;
+}
+
+exports.diff = function diff(a, b) {
+    return a - b;
+}
+
+exports.product = function product(a, b) {
+    return a * b;
+}
